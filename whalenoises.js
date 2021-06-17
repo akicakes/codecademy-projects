510928c327600de95e4090cef0ec7e36056a4698
Aky Mac Donald <13792069+solidquartz@users.noreply.github.com>
2021-06-17 13:19:19 -0600

    Update whalenoises.js

--- whalenoises.js
+++ whalenoises.js
@@ -5,9 +5,7 @@ const vowels = ['a', 'i', 'u', 'e', 'o'];
 let resultArray = [];
 
 for (let i = 0; i < input.length; i++) {
-  //console.log('i is ' + i);
   for (let j = 0; j < vowels.length; j++) {
-    //console.log('j is ' + j);
     if (input[i] === vowels[j]) {
       resultArray.push(input[i])
     };
